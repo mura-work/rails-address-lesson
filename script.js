014c9cf6da8ff428a27fc2295734593e3e3ba254
murata <murata.naoya.infratop@gmail.com>
2019-10-14 22:20:42 +0900

    js実装1

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-var map;
-var marker;
-var infoWindow;
-var center = {
-	lat: 34.7019399,
-	lng: 135.51002519999997
-};
-function initMap() {
-	map = new google.maps.Map(document.getElementById('sample'), {
-		center: center,
-		zoom: 19
-	});
+// var map;
+// var marker;
+// var infoWindow;
+// var center = {
+// 	lat: 34.7019399,
+// 	lng: 135.51002519999997
+// };
+// function initMap() {
+// 	map = new google.maps.Map(document.getElementById('sample'), {
+// 		center: center,
+// 		zoom: 19
+// 	});
 
-	marker = new google.maps.Marker({
-		position: center,
-		map: map
-	});
+// 	marker = new google.maps.Marker({
+// 		position: center,
+// 		map: map
+// 	});
 
-	infoWindow = new google.maps.infoWindow({
-		content: '<div class="sample">TAM 大阪</div>'
-	});
+// 	infoWindow = new google.maps.infoWindow({
+// 		content: '<div class="sample">TAM 大阪</div>'
+// 	});
 
-	marker.addListener('click', function() {
-		infoWindow.open(map, marker);
-	});
-}
+// 	marker.addListener('click', function() {
+// 		infoWindow.open(map, marker);
+// 	});
+// }
 
 
